75ab107a6ca5ba45c71d4a62503ce42d2ebf9b08
Logan Hill <lthill0203@gmail.com>
2024-05-13 16:01:52 -0500

    TODO: Filter out the `Vacant Title` from the `CHAMPIONS` array. Also, remove the corresponding index from the `BELTS` array.

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 import { BELTS, CHAMPIONS } from "./src/data";
 
 console.log(BELTS, CHAMPIONS);
+
+const filteredChampions = CHAMPIONS.filter(
+  (champion) => champion !== "Vacant Title",
+);
+const filteredBelts = BELTS.filter(
+  (belt) =>
+    belt !== "World Tag Team Champions" &&
+    belt !== "SmackDown Tag Team Champions",
+);
