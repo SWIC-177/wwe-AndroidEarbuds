dbba9742554d4aa30898f1f21f5f97701dc49711
Logan Hill <lthill0203@gmail.com>
2024-05-13 16:05:29 -0500

    TODO: Filter out the `Tag Team Champions` from the `BELTS` array. Also, remove the corresponding indices from the `CHAMPIONS` array.

--- main.js
+++ main.js
@@ -10,3 +10,12 @@ const filteredBelts = BELTS.filter(
     belt !== "World Tag Team Champions" &&
     belt !== "SmackDown Tag Team Champions",
 );
+
+const tagTeamIndices = BELTS.reduce((acc, belt, index) => {
+  if (belt.includes("Tag Team Champions")) acc.push(index);
+  return acc;
+}, []);
+
+const filteredChampionsWithoutTagTeam = CHAMPIONS.filter(
+  (champion, index) => !tagTeamIndices.includes(index),
+);
